d30e91bfe4d62dfaa12eb2d95a7f5404800feaab
Hasan Sheikh <100096979+hasshe94@users.noreply.github.com>
2023-03-11 23:32:49 +0000

    Basic attack is still the only attack that works, advanced and special doesnt.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 import random
 from discord.ext import commands
 
-TOKEN = "MTA4MDk2MDkzOTI1MzMwNTQ2Nw.GI_Ali.dSs1C0Bluq3Pgss7yekU6DCgLq0FfMlNzvxFvk"
+TOKEN = "MTA4MDk2MDkzOTI1MzMwNTQ2Nw.GalbxG.Phl-agcaJtvJg1pUkP816dzfujDER_gf9nrrQc"
 
 intents = discord.Intents.default()
 intents.message_content = True
