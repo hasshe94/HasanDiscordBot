eca523caeea0da15d7c92e061cdd9dea1201f548
Hasan Sheikh <100096979+hasshe94@users.noreply.github.com>
2023-03-12 02:36:46 +0000

    made the invalid attack type statement work by making the attack_type a str

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 import random
 from discord.ext import commands
 
-TOKEN = "MTA4MDk2MDkzOTI1MzMwNTQ2Nw.GalbxG.Phl-agcaJtvJg1pUkP816dzfujDER_gf9nrrQc"
+TOKEN = "MTA4MDk2MDkzOTI1MzMwNTQ2Nw.GU1yjv.jHYTYtSDxXjXFgY0qvzH5StXlLSRkiKJ1g8OMU"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,19 +17,21 @@ enemy_hp = 200
 basic_attack = 20
 advanced_attack = 40
 special_attack = 60
+game_over = True
 
 #start attack function
 @client.command()
-async def attack(ctx, attack_type):
+async def attack(ctx, attack_type : str):
     """
     Check the guide for attack information
     """
     global user_hp
     global enemy_hp
+    global game_over
 
     # Check if the user entered a valid attack type
     if attack_type not in ["basic", "advanced", "special"]:
-        await ctx.send("Invalid attack type!")
+        await ctx.send("Invalid attack type! Enter !guide to learn how to play the game")
         return
 
     # Calculate the damage based on the attack type
@@ -60,14 +62,15 @@ async def attack(ctx, attack_type):
 
     # Check if the game is over
     if user_hp <= 0:
+        game_over = True
         await ctx.send(f"You lost the game!\nThe Enemy is at {enemy_hp} hp. \n Enter !start game to play again")
-        user_hp = 0
-        enemy_hp = 0
+      
     elif enemy_hp <= 0:
+        game_over = True
         await ctx.send("You won the game!\nThe Enemy is dead. \n Enter!start game to play again")
-        user_hp = 0
-        enemy_hp = 0
+
     elif user_hp > 0 and enemy_hp > 0:
+        game_over = False
         await ctx.send("The game is not over yet! Keep fighting!")
 
     
@@ -104,6 +107,10 @@ async def start(ctx):
     global enemy_hp
     user_hp = 100
     enemy_hp = 200
-    await ctx.send("Game started. Your health has been reset to 100, and the enemy's health has been reset to 200.")
+    await ctx.send("Game started. Your health has been reset to 100, and the enemy's health has been reset to 200.\n Enter !help for help on the game.")
 
+
+
+
+  
 client.run(TOKEN)
